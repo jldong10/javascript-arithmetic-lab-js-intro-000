ff8006ad9d1db0150daeafbc8c2d92ac2949eafe
steadfast-method-9892 <dong.jason86@gmail.com>
2020-10-06 05:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function decrement(n){
   return (n -= 1)
 }
 function makeInt(n){
-  return (parseInt('n', 10))
+  return parseInt('2', 10)
 }
 function preserveDecimal(n){
   return parseFloat('n')
